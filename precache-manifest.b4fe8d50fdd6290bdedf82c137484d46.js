3f30234670f54594d89c2dabd4687be2522277fc
lolloz98 <lorenzocarpaneto@yahoo.it>
2022-02-11 18:03:19 +0100

    Updates

---
+++ precache-manifest.b4fe8d50fdd6290bdedf82c137484d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ddff93ec658d836399a729630474c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc57f6749ed21cfa94a8",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "acbcec44ccc3112085be",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "dc57f6749ed21cfa94a8",
+    "url": "/static/js/2.5686cabb.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.5686cabb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbcec44ccc3112085be",
+    "url": "/static/js/main.fa63ac90.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
